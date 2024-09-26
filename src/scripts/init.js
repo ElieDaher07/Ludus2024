33f733244a92d3d3a6f79ece896cb8271f3c475f
ElieDaher07 <143806470+ElieDaher07@users.noreply.github.com>
2024-09-26 17:51:42 -0400

    Fin Devoir04 - Physics
    Creation de la carte Tiled au complet + Implementation du mouvement, de la physique, et le double saut.

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,13 +1,18 @@
 const config = {
   type: Phaser.AUTO,
   parent: "canvas-wrapper",
-  // transparent: true,
   pixelArt: true,
   width: 1280,
   height: 720,
   scene: [Accueil, Jeu, Credits, Victoire, PartieTerminee, Tutoriel],
   physics: {
-    default: "arcade"
+    default: "arcade",
+    arcade: {
+      debug: true,
+      gravity: {
+        y: 1000
+      }
+    }
   }
 };
 const game = new Phaser.Game(config);
--- src/scripts/scenes/jeu.js
+++ src/scripts/scenes/jeu.js
@@ -6,34 +6,55 @@ class Jeu extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image(
-      "bg",
-      "./assets/images/backgrounds/Rocky_Level/background1.png"
-    );
+    // Preload le(s) bouton(s)
 
     this.load.image("quit", "./assets/images/ui/Large_Buttons/Exit_Button.png");
 
     // Preload le Tiled map
-    this.load.tilemapTiledJSON("map", "./Tiled/rocky_world.json");
-    this.load.image("img_map", "./Tiled/rocky_world.png");
 
-    this.load.image("main_lev_build", "./Tiled/rocky_world.png");
+    this.load.tilemapTiledJSON(
+      "carte_json",
+      "./assets/images/backgrounds/Rocky_Level/carte_rocky.json"
+    );
+
+    this.load.image(
+      "background1_tile",
+      "./assets/images/backgrounds/Rocky_Level/background1.png"
+    );
+    this.load.image(
+      "background2_tile",
+      "./assets/images/backgrounds/Rocky_Level/background2.png"
+    );
+    this.load.image(
+      "background3_tile",
+      "./assets/images/backgrounds/Rocky_Level/background3.png"
+    );
+    this.load.image(
+      "background_main",
+      "./assets/images/backgrounds/Rocky_Level/main_lev_build.png"
+    );
+    this.load.image(
+      "background_other",
+      "./assets/images/backgrounds/Rocky_Level/other_lev_build.png"
+    );
+
+    // Preload le joueur
+
+    this.load.image("player", "./assets/images/characters/player/idle01.png")
   }
 
   create() {
-    // HUD
-    const hudContainer = this.add.container(0, 0).setDepth(1);
 
-    // Background
-    let img = this.add.image(config.width / 2, config.height / 2, "bg");
-    let scaleX = config.width / img.width;
-    let scaleY = config.height / img.height;
-    let scale = Math.max(scaleX, scaleY);
-    img.setScale(scale);
+    this.jumpCount = 0;
+    this.jumpKeyReleased = true;
+
+    // HUD
+
+    const hudContainer = this.add.container(0, 0).setDepth(1);
 
     // Boutons
     let quitBtn = this.add
-      .image(config.width / 2, config.height / 2, "quit")
+      .image(config.width / 2 + 520, config.height / 2 - 320, "quit")
       .setScale(0.3);
 
     hudContainer.add(quitBtn);
@@ -46,34 +67,186 @@ class Jeu extends Phaser.Scene {
     });
 
     // Tilemap
-    const maCarte = this.make.tilemap({ key: "map" });
+    const maCarte = this.make.tilemap({
+      key: "carte_json"
+    });
 
     // Tileset
-    const tileSet_sky = maCarte.addTilesetImage("test", "main_lev_build");
-    const tileSet_mainlvl = maCarte.addTilesetImage(
+    const background1 = maCarte.addTilesetImage(
+      "background1",
+      "background1_tile"
+    );
+
+    const background2 = maCarte.addTilesetImage(
+      "background2",
+      "background2_tile"
+    );
+    const background3 = maCarte.addTilesetImage(
+      "background3",
+      "background3_tile"
+    );
+    const main_lev_build = maCarte.addTilesetImage(
       "main_lev_build",
-      "main_lev_build"
+      "background_main"
+    );
+    const other_lev_build = maCarte.addTilesetImage(
+      "other_lev_build",
+      "background_other"
     );
 
-    // Calques
-    const bgLayer = maCarte.createLayer("background_sky", tileSet_sky, 0, 0);
-    const collisionLayer = maCarte.createLayer(
-      "background_lev_build",
-      tileSet_mainlvl,
+    // Calques background non collision
+    const background_sky = maCarte.createLayer("background_sky", [background1], 0, 0);
+
+    const background_sky_front = maCarte.createLayer(
+      "background_sky_front",
+      [background2, other_lev_build],
       0,
       0
     );
-    const nonCollisionLayer = maCarte.createLayer(
-      "non_collision_objects",
-      tileSet_mainlvl,
+
+
+
+    const background_behind04 = maCarte.createLayer(
+      "background_behind04",
+      [background3, main_lev_build],
       0,
       0
     );
 
-    collisionLayer.setCollisionByProperty({ collision: true });
 
-    // this.physics.add.collider(this.player, collisionLayer);
+    const background_behind03 = maCarte.createLayer(
+      "background_behind03",
+      [main_lev_build],
+      0,
+      0
+    );
+
+    const background_behind02 = maCarte.createLayer(
+      "background_behind02",
+      [main_lev_build],
+      0,
+      0
+    );
+
+
+    const background_behind01 = maCarte.createLayer(
+      "background_behind01",
+      [main_lev_build, other_lev_build],
+      0,
+      0
+    );
+
+    const background_front = maCarte.createLayer("background_front", [main_lev_build], 0, 0);
+
+    const background_vegetation = maCarte.createLayer(
+      "background_vegetation",
+      [main_lev_build],
+      0,
+      0
+    );
+
+
+
+    // Calques avec collision
+
+    const collisionLayer01 = maCarte.createLayer(
+      "background_main",
+      [main_lev_build],
+      0,
+      0
+    );
+    const collisionLayer02 = maCarte.createLayer(
+      "background_bridge",
+      [main_lev_build],
+      0,
+      0
+    );
+
+    collisionLayer01.setCollisionByProperty({
+      collision: true
+    });
+
+    collisionLayer02.setCollisionByProperty({
+      collision: true
+    });
+
+    // Joueur
+
+    this.player = this.physics.add.image(config.width / 2 - 600, config.height / 2 - 180, "player");
+
+    this.player.setBounce(0);
+    this.player.setScale(0.7);
+    this.player.setSize(25, 40);
+    this.player.setOffset(10, 20);
+
+    // Collision
+
+    this.physics.add.collider(this.player, collisionLayer01);
+    this.physics.add.collider(this.player, collisionLayer02);
+
+    // Touches
+
+    this.keys = this.input.keyboard.addKeys({
+      jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+
+
   }
 
-  update() {}
+  update() {
+
+    // Mouvement avec A et D
+
+    if (this.keys.left.isDown) {
+      this.player.body.setVelocityX(-160);
+    } else if (this.keys.right.isDown) {
+      this.player.body.setVelocityX(160);
+    } else {
+      this.player.body.setVelocityX(0);
+    }
+
+
+    // Double saut
+
+    if (this.keys.jump.isUp) {
+      this.jumpKeyReleased = true; // La touche est relâchée
+    }
+    if (this.keys.jump.isDown && this.jumpKeyReleased && (this.player.body.onFloor() || this.jumpCount < 1)) { // touching down et blocked down peuvent pas marcher, j'ai cherché en ligne pour trouver onFloor()
+
+      this.player.setVelocityY(-300);
+      this.jumpCount++;
+      this.jumpKeyReleased = false; // Empêche de maintenir la touche pour continuer à sauter
+    }
+
+    // Reset le jumpcount
+
+    if (this.player.body.onFloor()) {
+      this.jumpCount = 0;
+    }
+
+
+    // Contraintes de deplacement
+
+    const playerWidth = 25;
+    const playerHeight = 40;
+
+    if (this.player.x < playerWidth / 2) {
+      this.player.x = playerWidth / 2;
+    } else if (this.player.x > this.scale.width - playerWidth / 2) {
+      this.player.x = this.scale.width - playerWidth / 2;
+    }
+
+    if (this.player.y < playerHeight / 2) {
+      this.player.y = playerHeight / 2;
+    } else if (this.player.y > this.scale.height - playerHeight / 2) {
+      this.player.y = this.scale.height - playerHeight / 2;
+    }
+
+  }
+
+
+
+
 }
